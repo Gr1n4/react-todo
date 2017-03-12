48dac81cea0bde7854ec1ca410bd1e5cb21e7847
Dmitry <lead@w.wilix.ru>
2017-03-12 03:27:28 +0300

    initial

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import './assets/sass/index.sass';
+
+import {App} from './app/app.component';
+console.log('test');
+
+ReactDOM.render(<App />, document.getElementById('root'));
