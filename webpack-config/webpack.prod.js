08f39dbdd86639638c0225483df92691d6dcddbd
Alex <luchcko.a@gmail.com>
2018-03-13 13:00:27 +0300

    update packages and some fix with build

--- webpack-config/webpack.prod.js
+++ webpack-config/webpack.prod.js
@@ -1,18 +1,9 @@
-const webpack = require('webpack');
-const webpackMerge = require('webpack-merge');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const commonConfig = require('./webpack.common.js');
-const path = require('path');
-const _root = path.resolve(__dirname, '..');
-function root(args) {
-  args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [_root].concat(args));
-}
-const helpers = {
-  root: root
-};
-
-const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+const webpack = require('webpack')
+    , webpackMerge = require('webpack-merge')
+    , ExtractTextPlugin = require('extract-text-webpack-plugin')
+    , commonConfig = require('./webpack.common.js')
+    , helpers = require('./helpers')
+    , ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'none',
@@ -26,7 +17,7 @@ module.exports = webpackMerge(commonConfig, {
 
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
+    new webpack.optimize.UglifyJsPlugin({
       mangle: {
         keep_fnames: true
       }
